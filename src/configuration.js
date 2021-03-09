901c75ab199a81d12e6135637ff265e803b5d8fd
Kévin Barfleur <kevinbarfleur@gmail.com>
2021-03-09 14:48:52 +0100

    Adding style for all tags

---
+++ src/configuration.js
@@ -0,0 +1,543 @@
+const H1 = {
+    fontSize: {
+        type: 'Length',
+        label: {
+            en: 'Size',
+            fr: 'Taille',
+        },
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 1, max: 100 }],
+        },
+        path: 'h1.fontSize',
+    },
+    fontFamily: {
+        type: 'FontFamily',
+        label: {
+            en: 'Font family',
+            fr: 'Font',
+        },
+        path: 'h1.fontFamily',
+    },
+    fontWeight: {
+        label: {
+            en: 'Font weight',
+            fr: 'Graisse',
+        },
+        type: 'TextSelect',
+        options: {
+            options: [
+                { value: null, label: { en: 'Default', fr: 'Par défaut' } },
+                { value: 100, label: { en: '100 - Thin' } },
+                { value: 200, label: { en: '200 - Extra Light' } },
+                { value: 300, label: { en: '300 - Light' } },
+                { value: 400, label: { en: '400 - Normal' } },
+                { value: 500, label: { en: '500 - Medium' } },
+                { value: 600, label: { en: '600 - Semi Bold' } },
+                { value: 700, label: { en: '700 - Bold' } },
+                { value: 800, label: { en: '800 - Extra Bold' } },
+                { value: 900, label: { en: '900 - Black' } },
+            ],
+        },
+        path: 'h1.fontWeight',
+    },
+    textAlign: {
+        type: 'TextRadioGroup',
+        options: {
+            choices: [
+                {
+                    value: 'left',
+                    default: true,
+                    title: { en: 'Left', fr: 'Gauche' },
+                    icon: 'text-align-left',
+                },
+                {
+                    value: 'center',
+                    title: { en: 'Center', fr: 'Milieu' },
+                    icon: 'text-align-center',
+                },
+                {
+                    value: 'right',
+                    title: { en: 'Right', fr: 'Droite' },
+                    icon: 'text-align-right',
+                },
+                {
+                    value: 'justify',
+                    title: { en: 'Justify', fr: 'Justifié' },
+                    icon: 'text-align-justify',
+                },
+            ],
+        },
+        label: {
+            en: 'Alignement',
+            fr: 'Alignement',
+        },
+        path: 'h1.textAlign',
+    },
+    color: {
+        type: 'Color',
+        label: {
+            en: 'Text color',
+            fr: 'Couleur du texte',
+        },
+        bindable: true,
+        options: {
+            nullable: true,
+        },
+        path: 'h1.color',
+    },
+    lineHeight: {
+        label: {
+            en: 'Line height',
+            fr: 'Hauteur de ligne',
+        },
+        type: 'Length',
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 0, max: 100 }],
+        },
+        path: 'h1.lineHeight',
+    },
+};
+
+const H2 = {
+    fontSize: {
+        type: 'Length',
+        label: {
+            en: 'Size',
+            fr: 'Taille',
+        },
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 1, max: 100 }],
+        },
+        path: 'h2.fontSize',
+    },
+    fontFamily: {
+        type: 'FontFamily',
+        label: {
+            en: 'Font family',
+            fr: 'Font',
+        },
+        path: 'h2.fontFamily',
+    },
+    fontWeight: {
+        label: {
+            en: 'Font weight',
+            fr: 'Graisse',
+        },
+        type: 'TextSelect',
+        options: {
+            options: [
+                { value: null, label: { en: 'Default', fr: 'Par défaut' } },
+                { value: 100, label: { en: '100 - Thin' } },
+                { value: 200, label: { en: '200 - Extra Light' } },
+                { value: 300, label: { en: '300 - Light' } },
+                { value: 400, label: { en: '400 - Normal' } },
+                { value: 500, label: { en: '500 - Medium' } },
+                { value: 600, label: { en: '600 - Semi Bold' } },
+                { value: 700, label: { en: '700 - Bold' } },
+                { value: 800, label: { en: '800 - Extra Bold' } },
+                { value: 900, label: { en: '900 - Black' } },
+            ],
+        },
+        path: 'h2.fontWeight',
+    },
+    textAlign: {
+        type: 'TextRadioGroup',
+        options: {
+            choices: [
+                {
+                    value: 'left',
+                    default: true,
+                    title: { en: 'Left', fr: 'Gauche' },
+                    icon: 'text-align-left',
+                },
+                {
+                    value: 'center',
+                    title: { en: 'Center', fr: 'Milieu' },
+                    icon: 'text-align-center',
+                },
+                {
+                    value: 'right',
+                    title: { en: 'Right', fr: 'Droite' },
+                    icon: 'text-align-right',
+                },
+                {
+                    value: 'justify',
+                    title: { en: 'Justify', fr: 'Justifié' },
+                    icon: 'text-align-justify',
+                },
+            ],
+        },
+        label: {
+            en: 'Alignement',
+            fr: 'Alignement',
+        },
+        path: 'h2.textAlign',
+    },
+    color: {
+        type: 'Color',
+        label: {
+            en: 'Text color',
+            fr: 'Couleur du texte',
+        },
+        bindable: true,
+        options: {
+            nullable: true,
+        },
+        path: 'h2.color',
+    },
+    lineHeight: {
+        label: {
+            en: 'Line height',
+            fr: 'Hauteur de ligne',
+        },
+        type: 'Length',
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 0, max: 100 }],
+        },
+        path: 'h2.lineHeight',
+    },
+};
+
+const H3 = {
+    fontSize: {
+        type: 'Length',
+        label: {
+            en: 'Size',
+            fr: 'Taille',
+        },
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 1, max: 100 }],
+        },
+        path: 'h3.fontSize',
+    },
+    fontFamily: {
+        type: 'FontFamily',
+        label: {
+            en: 'Font family',
+            fr: 'Font',
+        },
+        path: 'h3.fontFamily',
+    },
+    fontWeight: {
+        label: {
+            en: 'Font weight',
+            fr: 'Graisse',
+        },
+        type: 'TextSelect',
+        options: {
+            options: [
+                { value: null, label: { en: 'Default', fr: 'Par défaut' } },
+                { value: 100, label: { en: '100 - Thin' } },
+                { value: 200, label: { en: '200 - Extra Light' } },
+                { value: 300, label: { en: '300 - Light' } },
+                { value: 400, label: { en: '400 - Normal' } },
+                { value: 500, label: { en: '500 - Medium' } },
+                { value: 600, label: { en: '600 - Semi Bold' } },
+                { value: 700, label: { en: '700 - Bold' } },
+                { value: 800, label: { en: '800 - Extra Bold' } },
+                { value: 900, label: { en: '900 - Black' } },
+            ],
+        },
+        path: 'h3.fontWeight',
+    },
+    textAlign: {
+        type: 'TextRadioGroup',
+        options: {
+            choices: [
+                {
+                    value: 'left',
+                    default: true,
+                    title: { en: 'Left', fr: 'Gauche' },
+                    icon: 'text-align-left',
+                },
+                {
+                    value: 'center',
+                    title: { en: 'Center', fr: 'Milieu' },
+                    icon: 'text-align-center',
+                },
+                {
+                    value: 'right',
+                    title: { en: 'Right', fr: 'Droite' },
+                    icon: 'text-align-right',
+                },
+                {
+                    value: 'justify',
+                    title: { en: 'Justify', fr: 'Justifié' },
+                    icon: 'text-align-justify',
+                },
+            ],
+        },
+        label: {
+            en: 'Alignement',
+            fr: 'Alignement',
+        },
+        path: 'h3.textAlign',
+    },
+    color: {
+        type: 'Color',
+        label: {
+            en: 'Text color',
+            fr: 'Couleur du texte',
+        },
+        bindable: true,
+        options: {
+            nullable: true,
+        },
+        path: 'h3.color',
+    },
+    lineHeight: {
+        label: {
+            en: 'Line height',
+            fr: 'Hauteur de ligne',
+        },
+        type: 'Length',
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 0, max: 100 }],
+        },
+        path: 'h3.lineHeight',
+    },
+};
+
+const H4 = {
+    fontSize: {
+        type: 'Length',
+        label: {
+            en: 'Size',
+            fr: 'Taille',
+        },
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 1, max: 100 }],
+        },
+        path: 'h4.fontSize',
+    },
+    fontFamily: {
+        type: 'FontFamily',
+        label: {
+            en: 'Font family',
+            fr: 'Font',
+        },
+        path: 'h4.fontFamily',
+    },
+    fontWeight: {
+        label: {
+            en: 'Font weight',
+            fr: 'Graisse',
+        },
+        type: 'TextSelect',
+        options: {
+            options: [
+                { value: null, label: { en: 'Default', fr: 'Par défaut' } },
+                { value: 100, label: { en: '100 - Thin' } },
+                { value: 200, label: { en: '200 - Extra Light' } },
+                { value: 300, label: { en: '300 - Light' } },
+                { value: 400, label: { en: '400 - Normal' } },
+                { value: 500, label: { en: '500 - Medium' } },
+                { value: 600, label: { en: '600 - Semi Bold' } },
+                { value: 700, label: { en: '700 - Bold' } },
+                { value: 800, label: { en: '800 - Extra Bold' } },
+                { value: 900, label: { en: '900 - Black' } },
+            ],
+        },
+        path: 'h4.fontWeight',
+    },
+    textAlign: {
+        type: 'TextRadioGroup',
+        options: {
+            choices: [
+                {
+                    value: 'left',
+                    default: true,
+                    title: { en: 'Left', fr: 'Gauche' },
+                    icon: 'text-align-left',
+                },
+                {
+                    value: 'center',
+                    title: { en: 'Center', fr: 'Milieu' },
+                    icon: 'text-align-center',
+                },
+                {
+                    value: 'right',
+                    title: { en: 'Right', fr: 'Droite' },
+                    icon: 'text-align-right',
+                },
+                {
+                    value: 'justify',
+                    title: { en: 'Justify', fr: 'Justifié' },
+                    icon: 'text-align-justify',
+                },
+            ],
+        },
+        label: {
+            en: 'Alignement',
+            fr: 'Alignement',
+        },
+        path: 'h4.textAlign',
+    },
+    color: {
+        type: 'Color',
+        label: {
+            en: 'Text color',
+            fr: 'Couleur du texte',
+        },
+        bindable: true,
+        options: {
+            nullable: true,
+        },
+        path: 'h4.color',
+    },
+    lineHeight: {
+        label: {
+            en: 'Line height',
+            fr: 'Hauteur de ligne',
+        },
+        type: 'Length',
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 0, max: 100 }],
+        },
+        path: 'h4.lineHeight',
+    },
+};
+
+const P = {
+    fontSize: {
+        type: 'Length',
+        label: {
+            en: 'Size',
+            fr: 'Taille',
+        },
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 1, max: 100 }],
+        },
+        path: 'p.fontSize',
+    },
+    fontFamily: {
+        type: 'FontFamily',
+        label: {
+            en: 'Font family',
+            fr: 'Font',
+        },
+        path: 'p.fontFamily',
+    },
+    textAlign: {
+        type: 'TextRadioGroup',
+        options: {
+            choices: [
+                {
+                    value: 'left',
+                    default: true,
+                    title: { en: 'Left', fr: 'Gauche' },
+                    icon: 'text-align-left',
+                },
+                {
+                    value: 'center',
+                    title: { en: 'Center', fr: 'Milieu' },
+                    icon: 'text-align-center',
+                },
+                {
+                    value: 'right',
+                    title: { en: 'Right', fr: 'Droite' },
+                    icon: 'text-align-right',
+                },
+                {
+                    value: 'justify',
+                    title: { en: 'Justify', fr: 'Justifié' },
+                    icon: 'text-align-justify',
+                },
+            ],
+        },
+        label: {
+            en: 'Alignement',
+            fr: 'Alignement',
+        },
+        path: 'p.textAlign',
+    },
+    color: {
+        type: 'Color',
+        label: {
+            en: 'Text color',
+            fr: 'Couleur du texte',
+        },
+        bindable: true,
+        options: {
+            nullable: true,
+        },
+        path: 'p.color',
+    },
+    lineHeight: {
+        label: {
+            en: 'Line height',
+            fr: 'Hauteur de ligne',
+        },
+        type: 'Length',
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 0, max: 100 }],
+        },
+        path: 'p.lineHeight',
+    },
+};
+
+const A = {
+    fontSize: {
+        type: 'Length',
+        label: {
+            en: 'Size',
+            fr: 'Taille',
+        },
+        options: {
+            unitChoices: [{ value: 'px', label: 'px', min: 1, max: 100 }],
+        },
+        path: 'a.fontSize',
+    },
+    fontFamily: {
+        type: 'FontFamily',
+        label: {
+            en: 'Font family',
+            fr: 'Font',
+        },
+        path: 'a.fontFamily',
+    },
+    color: {
+        type: 'Color',
+        label: {
+            en: 'Text color',
+            fr: 'Couleur du texte',
+        },
+        bindable: true,
+        options: {
+            nullable: true,
+        },
+        path: 'a.color',
+    },
+    isUnderline: {
+        type: 'OnOff',
+        label: {
+            en: 'Underline',
+            fr: 'Soulingé',
+        },
+        path: 'a.isUnderline',
+    },
+};
+
+export const getSettingsConfigurations = content => {
+    switch (content.selectedTag) {
+        case 'h1':
+            return { styleOptions: { ...H1 } };
+        case 'h2':
+            return { styleOptions: { ...H2 } };
+        case 'h3':
+            return { styleOptions: { ...H3 } };
+        case 'h4':
+            return { styleOptions: { ...H4 } };
+        case 'h5':
+            return { styleOptions: { ...H1 } };
+        case 'h6':
+            return { styleOptions: { ...H1 } };
+        case 'p':
+            return { styleOptions: { ...P } };
+        case 'a':
+            return { styleOptions: { ...A } };
+
+        default:
+            return { styleOptions: { ...P } };
+    }
+};
--- src/wwElement.vue
+++ src/wwElement.vue
@@ -11,6 +11,7 @@
 
 <script>
 import showdown from 'showdown';
+import { getSettingsConfigurations } from './configuration';
 
 export default {
     name: '__COMPONENT_NAME__',
@@ -24,34 +25,143 @@ export default {
         text: {
             en: '',
         },
-        fontSize: wwLib.responsive('16px'),
-        titleFont: wwLib.responsive(''),
-        titleWeight: wwLib.responsive(''),
-        fontFamily: wwLib.responsive(''),
-        textAlign: wwLib.responsive(''),
-        color: wwLib.responsive(''),
-        lineHeight: wwLib.responsive(''),
-        linksColor: 'rgb(0, 125, 255)',
+        selectedTag: 'p',
+        h1: {
+            fontSize: wwLib.responsive('32px'),
+            fontFamily: wwLib.responsive(''),
+            fontWeight: wwLib.responsive(''),
+            textAlign: wwLib.responsive(''),
+            color: wwLib.responsive(''),
+            lineHeight: wwLib.responsive('32px'),
+        },
+        h2: {
+            fontSize: wwLib.responsive('24px'),
+            fontFamily: wwLib.responsive(''),
+            fontWeight: wwLib.responsive(''),
+            textAlign: wwLib.responsive(''),
+            color: wwLib.responsive(''),
+            lineHeight: wwLib.responsive('32px'),
+        },
+        h3: {
+            fontSize: wwLib.responsive('19px'),
+            fontFamily: wwLib.responsive(''),
+            fontWeight: wwLib.responsive(''),
+            textAlign: wwLib.responsive(''),
+            color: wwLib.responsive(''),
+            lineHeight: wwLib.responsive('32px'),
+        },
+        h4: {
+            fontSize: wwLib.responsive('16px'),
+            fontFamily: wwLib.responsive(''),
+            fontWeight: wwLib.responsive(''),
+            textAlign: wwLib.responsive(''),
+            color: wwLib.responsive(''),
+            lineHeight: wwLib.responsive('32px'),
+        },
+        h5: {
+            fontSize: wwLib.responsive('13px'),
+            fontFamily: wwLib.responsive(''),
+            fontWeight: wwLib.responsive(''),
+            textAlign: wwLib.responsive(''),
+            color: wwLib.responsive(''),
+            lineHeight: wwLib.responsive('32px'),
+        },
+        h6: {
+            fontSize: wwLib.responsive('10px'),
+            fontFamily: wwLib.responsive(''),
+            fontWeight: wwLib.responsive(''),
+            textAlign: wwLib.responsive(''),
+            color: wwLib.responsive(''),
+            lineHeight: wwLib.responsive('32px'),
+        },
+        p: {
+            fontSize: wwLib.responsive('16px'),
+            fontFamily: wwLib.responsive(''),
+            fontWeight: wwLib.responsive(''),
+            textAlign: wwLib.responsive(''),
+            color: wwLib.responsive(''),
+            lineHeight: wwLib.responsive('25px'),
+        },
+        a: {
+            fontSize: wwLib.responsive('16px'),
+            fontFamily: wwLib.responsive(''),
+            color: wwLib.responsive(''),
+            isUnderline: true,
+        },
     },
     data() {
         return {
             converter: new showdown.Converter(),
         };
     },
+    /* wwEditor:start */
+    wwEditorConfiguration({ content }) {
+        return getSettingsConfigurations(content);
+    },
+    /* wwEditor:end */
     computed: {
         html() {
             return this.converter.makeHtml(wwLib.wwLang.getText(this.content.text));
         },
         style() {
             return {
-                fontSize: this.content.fontSize,
-                fontFamily: this.content.fontFamily,
-                textAlign: this.content.textAlign,
-                color: this.content.color,
-                lineHeight: this.content.lineHeight,
-                '--links-color': this.content.linksColor,
-                '--title-font-family': this.content.titleFont,
-                '--title-font-weight': this.content.titleWeight,
+                // H1
+                '--h1-fontSize': this.content.h1.fontSize,
+                '--h1-fontFamily': this.content.h1.fontFamily,
+                '--h1-fontWeight': this.content.h1.fontWeight,
+                '--h1-textAlign': this.content.h1.textAlign,
+                '--h1-color': this.content.h1.color,
+                '--h1-lineHeight': this.content.h1.lineHeight,
+                // H2
+                '--h2-fontSize': this.content.h2.fontSize,
+                '--h2-fontFamily': this.content.h2.fontFamily,
+                '--h2-fontWeight': this.content.h2.fontWeight,
+                '--h2-textAlign': this.content.h2.textAlign,
+                '--h2-color': this.content.h2.color,
+                '--h2-lineHeight': this.content.h2.lineHeight,
+                // H3
+                '--h3-fontSize': this.content.h3.fontSize,
+                '--h3-fontFamily': this.content.h3.fontFamily,
+                '--h3-fontWeight': this.content.h3.fontWeight,
+                '--h3-textAlign': this.content.h3.textAlign,
+                '--h3-color': this.content.h3.color,
+                '--h3-lineHeight': this.content.h3.lineHeight,
+                // H4
+                '--h4-fontSize': this.content.h4.fontSize,
+                '--h4-fontFamily': this.content.h4.fontFamily,
+                '--h4-fontWeight': this.content.h4.fontWeight,
+                '--h4-textAlign': this.content.h4.textAlign,
+                '--h4-color': this.content.h4.color,
+                '--h4-lineHeight': this.content.h4.lineHeight,
+                // H5
+                '--h5-fontSize': this.content.h3.fontSize,
+                '--h5-fontFamily': this.content.h3.fontFamily,
+                '--h5-fontWeight': this.content.h3.fontWeight,
+                '--h5-textAlign': this.content.h3.textAlign,
+                '--h5-color': this.content.h3.color,
+                '--h5-lineHeight': this.content.h3.lineHeight,
+                // H6
+                '--h6-fontSize': this.content.h6.fontSize,
+                '--h6-fontFamily': this.content.h6.fontFamily,
+                '--h6-fontWeight': this.content.h6.fontWeight,
+                '--h6-textAlign': this.content.h6.textAlign,
+                '--h6-color': this.content.h6.color,
+                '--h6-lineHeight': this.content.h6.lineHeight,
+                // p
+                '--p-fontSize': this.content.p.fontSize,
+                '--p-fontFamily': this.content.p.fontFamily,
+                '--p-fontWeight': this.content.p.fontWeight,
+                '--p-textAlign': this.content.p.textAlign,
+                '--p-color': this.content.p.color,
+                '--p-lineHeight': this.content.p.lineHeight,
+                // a
+                '--a-fontSize': this.content.a.fontSize,
+                '--a-fontFamily': this.content.a.fontFamily,
+                '--a-fontWeight': this.content.a.fontWeight,
+                '--a-textAlign': this.content.a.textAlign,
+                '--a-color': this.content.a.color,
+                '--a-lineHeight': this.content.a.lineHeight,
+                '--a-underline': this.content.a.isUnderline ? 'underline' : 'none',
             };
         },
     },
@@ -72,24 +182,79 @@ export default {
 
 <style lang="scss">
 .ww-rich-text-temp {
-    --links-color: rgb(0, 125, 255);
-    --title-font-family: 'Roboto';
-    --title-font-weight: 400;
-
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
+    h1 {
+        font-size: var(--h1-fontSize);
+        font-family: var(--h1-fontFamily);
+        font-weight: var(--h1-fontSize);
+        text-align: var(--h1-textAlign);
+        color: var(--h1-color);
+        line-height: var(--h1-lineHeight);
+    }
+    h2 {
+        font-size: var(--h2-fontSize);
+        font-family: var(--h2-fontFamily);
+        font-weight: var(--h2-fontSize);
+        text-align: var(--h2-textAlign);
+        color: var(--h2-color);
+        line-height: var(--h2-lineHeight);
+    }
+    h3 {
+        font-size: var(--h3-fontSize);
+        font-family: var(--h3-fontFamily);
+        font-weight: var(--h3-fontSize);
+        text-align: var(--h3-textAlign);
+        color: var(--h3-color);
+        line-height: var(--h3-lineHeight);
+    }
+    h4 {
+        font-size: var(--h4-fontSize);
+        font-family: var(--h4-fontFamily);
+        font-weight: var(--h4-fontSize);
+        text-align: var(--h4-textAlign);
+        color: var(--h4-color);
+        line-height: var(--h4-lineHeight);
+    }
+    h5 {
+        font-size: var(--h5-fontSize);
+        font-family: var(--h5-fontFamily);
+        font-weight: var(--h5-fontSize);
+        text-align: var(--h5-textAlign);
+        color: var(--h5-color);
+        line-height: var(--h5-lineHeight);
+    }
     h6 {
-        font-family: var(--title-font-family);
-        font-weight: var(--title-font-weight);
+        font-size: var(--h6-fontSize);
+        font-family: var(--h6-fontFamily);
+        font-weight: var(--h6-fontSize);
+        text-align: var(--h6-textAlign);
+        color: var(--h6-color);
+        line-height: var(--h6-lineHeight);
+    }
+    p {
+        font-size: var(--p-fontSize);
+        font-family: var(--p-fontFamily);
+        font-weight: var(--p-fontSize);
+        text-align: var(--p-textAlign);
+        color: var(--p-color);
+        line-height: var(--p-lineHeight);
     }
     a {
         display: initial;
-        color: var(--links-color);
-        text-decoration: underline;
+        text-decoration: var(--a-underline);
+        font-size: var(--a-fontSize);
+        font-family: var(--a-fontFamily);
+        font-weight: var(--a-fontSize);
+        text-align: var(--a-textAlign);
+        color: var(--a-color);
+        line-height: var(--a-lineHeight);
     }
+    font-size: var(--p-fontSize);
+    font-family: var(--p-fontFamily);
+    font-weight: var(--p-fontSize);
+    text-align: var(--p-textAlign);
+    color: var(--p-color);
+    line-height: var(--p-lineHeight);
+
     img,
     video,
     iframe {
